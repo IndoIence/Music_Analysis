e49783277f188c5f99b28d36e394283749727ad0
tadeu <tadeusz.plesniak@gmail.com>
2023-08-02 09:48:12 +0200

    Genius Scraping

--- notebooks/Analysis_imports.py
+++ notebooks/Analysis_imports.py
@@ -1,13 +1,26 @@
-import re
-import nltk
-import lyricsgenius
+import logging
 import pickle
-from API_KEYS import GENIUS_BEARER as token
+import re
 from itertools import chain
+
+import lyricsgenius
+import nltk
+
+from config import GENIUS_BEARER as token
+from config import LOGFILE, GENIUS_TIMEOUT, GENIUS_SLEEP_TIME
+
+logging.basicConfig(
+    filename=LOGFILE,
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s"
+)
+
+
 def clean_song_text(lyrics):
     def clean_brackets(text):
         pattern = r'\[.*?\]'
-        return re.sub(pattern,'', text)
+        return re.sub(pattern, '', text)
+
     # find the first occurance of the word "Lyrics", and discard what's before that
     lyrics_start = lyrics.find('Lyrics') + len('Lyrics')
     lyrics = lyrics[lyrics_start:].lower()
@@ -30,24 +43,46 @@ def clean_song_text(lyrics):
     # lyrics = list(filter(None, lyrics))
     return lyrics
 
-def scrape_artist_songs(artist_list, max_songs=150, verbose=False, save_small_artists=True):
-    genius = lyricsgenius.Genius(token, verbose=verbose)
+
+def names_very_different(lastfm_name, genius_name):
+    # if no substring longer than 3 is from the original name, then that name is different
+    # For the name Tau Genius returns 2 Chainz (???)
+    # want to see if in many cases name received from genius is completely different
+    # if the names are short don't consider
+    if len(lastfm_name) < 3 or len(genius_name) < 3:
+        return False
+    for i in range(len(lastfm_name) - 3):
+        if lastfm_name[i:i + 3].lower() in genius_name.lower():
+            return False
+    return True
+
+### TODO: try except for timeouts and other errors
+def scrape_artist_songs(artist_list, max_songs=2000, verbose=False, save_small_artists=True):
+    genius = lyricsgenius.Genius(token, verbose=verbose, timeout=GENIUS_TIMEOUT, sleep_time=GENIUS_SLEEP_TIME)
     scrape_folder_prefix = '../scraped_data/Genius/'
     for artist_name in artist_list:
+        logging.info(f"Beginning search for {artist_name}")
         artist = genius.search_artist(artist_name, max_songs=max_songs)
         # when the song search is empty it probably means that there are no songs in genius to scrape
         if not artist:
+            logging.info(f"Nothing found for: {artist_name}")
             continue
+        if names_very_different(artist_name, artist.name):
+            # changing name here is a bad practice and can be problematic in the future, but it is late and i am lazy to rewrite more
+            artist_name = artist_name + '(' + artist.name + ')'
+            print(f'Name from lastFM: {artist_name},\nName from Genius {artist.name}')
+            logging.warning(f'Name from lastFM: {artist_name}, Name from Genius {artist.name}')
         albums = genius.search_albums(artist_name)
         album_names = [album['result']['name'] for album in albums['sections'][0]['hits']]
+
         # when the artist doesn't have 3 albums  or 15 songs don't consider them
         if len(album_names) < 3:
             print(f'{artist_name} has only {len(album_names)} albums')
-            continue
+            logging.info(f'{artist_name} has only {len(album_names)} albums')
         nr_of_songs = len(artist.songs)
         if nr_of_songs < 15:
             print(f'artist has only {nr_of_songs} songs')
-            continue
+            logging.info(f'artist has only {nr_of_songs} songs')
         # Check the number of words in the songs. If there are below 30k save to the small artists
         songs_lyrics = [clean_song_text(song.lyrics) for song in artist.songs]
         word_count = count_words_in_lyrics(songs_lyrics)
@@ -57,6 +92,8 @@ def scrape_artist_songs(artist_list, max_songs=150, verbose=False, save_small_ar
         else:
             with open(scrape_folder_prefix + 'small_artists/' + artist_name, 'wb') as f:
                 pickle.dump(artist, f)
+        logging.info(f"Ending search for {artist_name}")
+
 
 def count_words_in_lyrics(songs):
     # better memory usage than len(list(chain(*songs))) i guess
