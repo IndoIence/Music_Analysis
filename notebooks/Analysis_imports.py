8daf7694706a3777b48367ae2f68c0a4488bfe34
tadeu <tadeusz.plesniak@gmail.com>
2023-09-04 09:33:29 +0200

    huh

--- notebooks/Analysis_imports.py
+++ notebooks/Analysis_imports.py
@@ -60,6 +60,7 @@ def names_very_different(lastfm_name, genius_name):
 ### TODO: try except for timeouts and other errors
 ### TODO: often genius outputs a well known english artist ie (Ariana Grande, Chief Keef) find a way to mitigate those
 ### TODO: handling timout exceptions and HTTPERRORs
+### TODO: validating the pathname
 ##
 def scrape_artist_songs(artist_list, max_songs=2000, verbose=False, save_small_artists=True):
     genius = lyricsgenius.Genius(token, verbose=verbose, timeout=GENIUS_TIMEOUT, sleep_time=GENIUS_SLEEP_TIME)
