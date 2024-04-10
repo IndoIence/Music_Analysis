2d52fc2055eb937fa53c50f6f5aa9c8b79319905
tadple <tadeusz.plesniak@gmail.com>
2024-04-10 16:29:08 +0200

    add url updating to scrape_artist_ songs

--- Analysis_imports.py
+++ Analysis_imports.py
@@ -34,19 +34,22 @@ def names_very_different(lastfm_name, genius_name):
 ### TODO: For the name Tau Genius returns 2 Chainz (???)
 def scrape_artists_songs(artist_list: list[str]):
     urls = open(config_G["GENIUS_URLS_PATH"], 'r').read().strip().split()
+    urls_set = set((url.lower() for url in urls))
     for artist_id, artist_name in enumerate(tqdm(artist_list)):
         artist = scrape_artist_songs(artist_name)
         if not artist:
             continue
         # check if already saved
-        if artist.url in urls:
+        if artist.url.strip().lower() in urls_set:
             logging.info(f"Artist url {artist.url} already saved")
             continue
+        urls_set.add(artist.url.strip().lower())
         out_path = Path(config_G["save_path"])
          # save to the general file
         # in the general directory the file names are without .artPkl
         # -> for now only MyArtists have that
         save_artist_to_file(out_path, artist, artist_name)
+        
         my_artist = MyArtist(artist)
         # save to specific pl / non pl directories
         if my_artist.language == "pl":
@@ -55,6 +58,11 @@ def scrape_artists_songs(artist_list: list[str]):
             out_path2 = Path(CONFIG['artists_non_pl_path'])
         file_name = sanitize_art_name(my_artist.name)
         save_artist_to_file(out_path2, my_artist, file_name)
+    if len(urls) != len(urls_set):
+        # new artist added so update of is needed in the all_urls.txt file
+        with open(config_G["GENIUS_URLS_PATH"], 'w') as f:
+            for url in urls_set:
+                f.write(url + '/n')
 
 
         
