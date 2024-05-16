63d112434bfc6f7de7d3d184532f8756ecdb1d93
tadple <tadeusz.plesniak@gmail.com>
2024-05-16 16:10:18 +0200

    add temporary py files

---
+++ tmtoolkit_first_steps.py
@@ -0,0 +1,74 @@
+# %%
+from utils import get_artists, CONFIG, sanitize_art_name 
+from pathlib import Path
+from tqdm import tqdm
+import tempfile
+import spacy
+import joblib
+import pickle
+nlp = spacy.load('pl_core_news_sm')
+from tmtoolkit.corpus import Corpus, Document, print_summary, tokens_table
+
+def get_art_documents():
+    def doc_labels(name: str):
+        n = 1
+        while True:
+            yield name + f'_{n}'
+            n+=1
+    in_dir = Path(CONFIG['artists_pl_path'])
+    artist_names = CONFIG["artist_names"]
+    for a_name in artist_names:
+        f_name = sanitize_art_name(a_name)
+        with open(in_dir / (f_name + '.artPkl'), 'rb') as f:
+            art = pickle.load(f)
+            d = doc_labels(art.name)
+            for song in art.songs:
+                yield (next(d), song.clean_song_lyrics)
+# %%
+# for tf idf i need first all the texts from all artists together on a corpus
+c_small = Corpus({k:v for k,v in get_art_documents()}, spacy_instance=nlp, max_workers=0.1)
+
+# %% learn how to save a corpus to a directory
+# just pickle it and load it from pickle
+CONFIG['corpus_path'] = 'output_data/corpus/'
+corpus_path = Path(CONFIG['corpus_path'])
+# with open(corpus_path / 'tmtoolkit_top30.pkl', 'wb') as f:
+#     pickle.dump(c_small, f)
+joblib.dump(c_small, 'tmtoolkit_top30.pkl')
+
+
+# %%
+
+from tmtoolkit.corpus import lemmatize, to_lowercase, dtm
+from tmtoolkit.bow.bow_stats import tfidf, sorted_terms_table
+
+lemmatize(c_small)
+to_lowercase(c_small)
+mat, doc_labels, vocab = dtm(c_small, return_doc_labels=True, return_vocab=True)
+tfidf_mat = tfidf(mat)
+top_tokens = sorted_terms_table(tfidf_mat, vocab, doc_labels, top_n=5)
+# %%
+# how can i join all the songs in my corpus?
+art_names = [sanitize_art_name(name) for name in CONFIG['artist_names']]
+join_dict = {name + '_joined' : name + '*' for name in art_names}
+from tmtoolkit.corpus import corpus_join_documents
+corpus_join_documents(c_small, join_dict, match_type='glob')
+# %%
+def save_corpus(corpus, file_path):
+    serializable_data = {
+        'tokens': corpus.tokens,
+        'doc_labels': corpus.doc_labels,
+        'metadata': corpus.metadata,
+        # Add other attributes as needed
+    }
+    joblib.dump(serializable_data, file_path)
+
+def load_corpus(file_path):
+    loaded_data = joblib.load(file_path)
+    corpus = Corpus()
+    corpus.tokens = loaded_data['tokens']
+    corpus.doc_labels = loaded_data['doc_labels']
+    corpus.metadata = loaded_data['metadata']
+    # Assign other attributes as needed
+    return corpus
+# %%
---
+++ wsd_initial_data.py
@@ -0,0 +1,89 @@
+# %%
+# get data from wsd
+import numpy as np
+import matplotlib.pyplot as plt
+from utils import get_wsd_data, get_stopwords
+
+def get_wsd_vals(no_stopwords=False, tresholds: tuple[int, int] = ()):
+    """wsd data shrunk down to tuple with (Artist_name, list(best_scores), list(words))"""
+    if no_stopwords:
+        stopwords = set(get_stopwords())
+    all_scores: list[tuple[str,list[float], list[str]]] = []
+    for a_name, songs in get_wsd_data():
+        # print(a_name)
+        scores: list[float] = []
+        words: list[str] = []
+        for wsd in songs:
+            for word in wsd:
+                if no_stopwords and word['word'].lower() in stopwords:
+                    continue
+                vals = [float(word['suggestions'][plwn_id]['score']) for plwn_id in word['suggestions']]
+                m = max(vals)
+                if tresholds and (tresholds[0] > m or m >= tresholds[1]):
+                    continue
+                scores.append(m)
+                words.append(word['word'])
+        all_scores.append((a_name, scores, words))
+    return all_scores
+
+for artist, songs in get_wsd_data():
+    
+
+# %%
+with_stop = get_wsd_vals()
+no_stop = get_wsd_vals(no_stopwords=True)
+
+
+# %%
+for ((a_name, scores, _), (a_name1, scores1, _)) in zip(with_stop, no_stop):
+    plt.hist(scores, bins=10)
+    plt.hist(scores1, bins=10)
+    plt.title(a_name)
+    plt.legend(['stop', 'no_stop'])
+    plt.show()
+    # %%
+for a_name, scores, _ in no_stop:
+    plt.hist(scores, bins=10)
+    plt.title(a_name)
+    plt.show()
+# %%
+
+# %%
+small_score = get_wsd_vals(tresholds=(-80, -40))
+big_score = get_wsd_vals(tresholds=(40,60), no_stopwords=True)
+for a_name, scores, _ in small_score:
+    plt.hist(scores, bins=10)
+    plt.title(a_name)
+    plt.show()
+# %%
+# Peja duża pewność
+artist_ind = 1
+peja_name, scores, words = big_score[artist_ind]
+print(f"{a_name} pewne")
+for s,w, _ in zip(scores, words, range(20)):
+    print(s,w)
+
+peja_name, scores, words = small_score[artist_ind]
+print(f"{a_name} niepewne")
+for s,w, _ in zip(scores, words, range(20)):
+    print(s,w)
+# %%
+# the same shit with removed stopwords
+
+for a_name, scores, _ in no_stop:
+    plt.hist(scores, bins=10)
+    plt.title(a_name)
+    plt.show()
+# %%
+print(no_stop[0])
+# %%
+avg_stop = np.average(with_stop[1][1])
+std_stop = np.std(with_stop[1][1])
+avg_no_stop = np.average(no_stop[1][1])
+std_no_stop = np.std(no_stop[1][1])
+# %%
+print(len(with_stop[1][1]), len(no_stop[1][1]))
+print(avg_no_stop, std_no_stop)
+print(avg_stop, std_stop)
+
+# %%
