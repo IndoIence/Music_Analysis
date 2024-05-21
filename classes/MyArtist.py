f306d47d29dafb63b9ce2cc2e91905431c139bef
tadple <tadeusz.plesniak@gmail.com>
2024-05-21 16:18:33 +0200

    pre-commit setup

--- classes/MyArtist.py
+++ classes/MyArtist.py
@@ -4,8 +4,8 @@ from pathlib import Path
 from nltk import word_tokenize
 from copy import deepcopy
 from pickle import dump
-# i don't know if scpacy_fastlang is actually needed
 
+# i don't know if scpacy_fastlang is actually needed
 
 
 class MyArtist(Artist):
@@ -14,44 +14,49 @@ class MyArtist(Artist):
         # is it ok to lazy import like that?
         import spacy
         import spacy_fastlang
+
         nlp = spacy.load("pl_core_news_lg")
-        nlp.add_pipe('language_detector')
-        return nlp    
+        nlp.add_pipe("language_detector")
+        return nlp
 
     def __init__(self, artist: Artist):
         self.__dict__ = artist.__dict__
         self.songs = self._get_my_songs()
         self.songs_languages = self.get_songs_languages()
         self.language: str = self.songs_languages[0][0]
-        self._lyrics_len_only_art =  self.lyrics_len_only_art
+        self._lyrics_len_only_art = self.lyrics_len_only_art
         self._lyrics_len_all = self.lyrics_len_all
         self._lyrics_len_prim_art = self.lyrics_len_prim_art
         # self.lyrics_len: int = self.lyrics_len
         # self.nlp_doc = sel    out = ''f._get_nlp_docs(nlp)
+
     @property
     def lyrics_len_only_art(self):
-        if not hasattr(self, '_lyrics_len_only_art'):
+        if not hasattr(self, "_lyrics_len_only_art"):
             self._lyrics_len_only_art = self.get_lyrics_len(only_art=True)
         return self._lyrics_len_only_art
+
     @property
     def lyrics_len_all(self):
-        if not hasattr(self, '_lyrics_len_all'):
+        if not hasattr(self, "_lyrics_len_all"):
             self._lyrics_len_all = self.get_lyrics_len(include_features=True)
         return self._lyrics_len_all
+
     @property
     def lyrics_len_prim_art(self):
-        if not hasattr(self, '_lyrics_len_prim_art'):
+        if not hasattr(self, "_lyrics_len_prim_art"):
             self._lyrics_len_prim_art = self.get_lyrics_len(include_features=False)
         return self._lyrics_len_prim_art
+
     @property
     def name_sanitized(self) -> str:
-        return  self.name.replace(' ', '_').replace(".", "_").replace('/', ' ').replace('?', ' ').strip()
+        return self.name.replace(" ", "_").replace(".", "_").replace("/", " ").replace("?", " ").strip()
 
     def get_songs_languages(self) -> list[tuple[str, int]]:
         nlp = self._init_nlp()
         if not self.songs:
-            return [('xx', -1)]
-        counter = {}
+            return [("xx", -1)]
+        counter: dict[str, int] = {}
         for song in self.songs:
             lang = nlp(song.lyrics)._.language
             song.language = lang
@@ -60,14 +65,15 @@ class MyArtist(Artist):
             else:
                 counter[lang] = 1
         return sorted(counter.items(), key=lambda x: x[1], reverse=True)
-# TODO: Is include features even necessary? Isn't it the same as only_art?
+
+    # TODO: Is include features even necessary? Isn't it the same as only_art?
     def get_lyrics_len(self, include_features=True, only_art=False) -> int:
         if not self.songs:
             return 0
         count = 0
         for song in self.songs:
             # if the song primary artists is the current artist then include it
-            if song.language != 'pl':
+            if song.language != "pl":
                 continue
             if only_art and song._body["featured_artists"]:
                 continue
@@ -76,9 +82,8 @@ class MyArtist(Artist):
             count += self.count_words(song.clean_song_lyrics)
         return count
 
-
-    def _get_my_songs(self)-> list[MySong]:
-        if not hasattr(self, 'songs') or not self.songs:
+    def _get_my_songs(self) -> list[MySong]:
+        if not hasattr(self, "songs") or not self.songs:
             return []
         output = []
         for song in self.songs:
@@ -87,8 +92,15 @@ class MyArtist(Artist):
             elif isinstance(song, Song):
                 output.append(MySong(song))
         return output
-    
-    def get_limit_songs(self, limit:int=None, prim_art: bool=False, only_art:bool=False, strict=False) -> list[MySong]:
+
+    def get_limit_songs(
+        # i don't know how to achieve a infinite int with type hints
+        self,
+        limit: int | float = float("inf"),
+        prim_art: bool = False,
+        only_art: bool = False,
+        strict: bool = False,
+    ) -> list[MySong]:
         """
         returns list of songs with the limit of words
         if strict == True the output can be none
@@ -97,57 +109,57 @@ class MyArtist(Artist):
         """
         if strict and self.lyrics_len_only_art < limit:
             return []
-        if not limit:
-            limit = float('inf')
         count = 0
         result = []
         for song in self.songs:
             # skip non-polish songs
-            if song.language != 'pl':
+            if song.language != "pl":
                 continue
             if only_art and song._body["featured_artists"]:
                 continue
             if prim_art and song._body["primary_artist"]["id"] != self.id:
-                continue 
+                continue
             words = word_tokenize(song.clean_song_lyrics)
             count += len(words)
             result.append(song)
             if count >= limit:
-                diff = count - limit
+                diff = int(count - limit)
                 if strict and diff > 0:
                     words = words[:-diff]
                     new_song = deepcopy(song)
-                    new_song.lyrics = ' '.join(words)
+                    new_song.lyrics = " ".join(words)
                 break
         return result
-    #this should rather be in the song i think
+
+    # this should rather be in the song i think
     def count_words(self, text: str) -> int:
         return len(word_tokenize(text))
-    
-    def save_lyrics(self, save_path: Path = Path(''), filename=None, extension='txt'):
-            """
-            Overwrites the lyricsgenius.artist.save_lyrics().
-            Gives sanitized lyrics without brackets and genius unwanted additions
-            """
-            if not filename:
-                filename = self.name_sanitized
-            filename += '.' + extension
-            with open(save_path / filename, 'w') as f:
-                for i,song in enumerate(self.songs):
-                    f.write(str(i+1)+'. '+song.title)
-                    f.write('\n\n')
-                    f.write(song.clean_song_lyrics)
-                    f.write('\n\n')
+
+    def save_lyrics(self, save_path: Path = Path(""), filename="", extension="txt"):
+        """
+        Overwrites the lyricsgenius.artist.save_lyrics().
+        Gives sanitized lyrics without brackets and genius unwanted additions
+        """
+        if not filename:
+            filename = self.name_sanitized
+        filename += "." + extension
+        with open(save_path / filename, "w") as f:
+            for i, song in enumerate(self.songs):
+                f.write(str(i + 1) + ". " + song.title)
+                f.write("\n\n")
+                f.write(song.clean_song_lyrics)
+                f.write("\n\n")
+
     def to_pickle(
         self,
-        out_path: Path, 
-        file_name: str = None,
-        suffix:str = '.artPkl',
-        ):
+        out_path: Path,
+        file_name: str = "",
+        suffix: str = ".artPkl",
+    ):
         if file_name is None:
             file_name = self.name_sanitized
-    # before saving check if there are any / or ? in the name -> if so replace them
-        if '/' in file_name or '?' in file_name:
-            file_name = file_name.replace('/', ' ').replace('?', ' ')
-        with open(out_path / (file_name + suffix), 'wb') as f:
+        # before saving check if there are any / or ? in the name -> if so replace them
+        if "/" in file_name or "?" in file_name:
+            file_name = file_name.replace("/", " ").replace("?", " ")
+        with open(out_path / (file_name + suffix), "wb") as f:
             dump(self, f)
--- utils.py
+++ utils.py
@@ -8,31 +8,35 @@ import os
 import yaml
 from tqdm import tqdm
 
-config_path = Path(__file__).parent / "config.yml" 
-CONFIG = yaml.safe_load(open(config_path, 'r'))
+config_path = Path(__file__).parent / "config.yaml"
+CONFIG = yaml.safe_load(open(config_path))
 # %%
 
-def get_artist(name: str, path: Path = Path(CONFIG["artists_pl_path"])) -> MyArtist:
-    return pickle.load(open(path / name, 'rb'))
 
-def get_artists(path: Path = Path(CONFIG["artists_pl_path"]) ):
+def get_artist(name: str, path: Path = Path(CONFIG["artists_pl_path"])) -> MyArtist:
+    return pickle.load(open(path / name, "rb"))
+
+
+def get_artists(path: Path = Path(CONFIG["artists_pl_path"])):
     art_paths = [f for f in os.listdir(path) if os.path.isfile(path / f)]
     for art_path in tqdm(art_paths):
         art = get_artist(art_path, path)
         yield art
+
 
 def get_all_urls():
     genius_path = Path(CONFIG["Genius_scraping"]["save_path"])
     urls = []
 
     for art_path in (f for f in genius_path.iterdir() if f.is_file()):
-        with open(art_path, 'rb+') as f:
+        with open(art_path, "rb+") as f:
             art = pickle.load(f)
             urls.append(art.url)
     return sorted(set(urls))
 
-def get_biggest_by_lyrics_len(n: int = 50, only_art = True) -> list[MyArtist]:
-    heap: list[tuple(int,int,MyArtist)] = []
+
+def get_biggest_by_lyrics_len(n: int = 50, only_art=True) -> list[MyArtist]:
+    heap: list[tuple[int, int, MyArtist]] = []
     for helper, artist in tqdm(enumerate(get_artists())):
         l = artist.lyrics_len_only_art if only_art else artist.lyrics_len_all
         if len(heap) < n:
@@ -42,21 +46,25 @@ def get_biggest_by_lyrics_len(n: int = 50, only_art = True) -> list[MyArtist]:
     return [artist for _, _, artist in sorted(heap, reverse=True)]
 
 
-def all_artists_genius(genius_path:Path=Path(CONFIG["Genius_scraping"]["save_path"])):
+def all_artists_genius(genius_path: Path = Path(CONFIG["Genius_scraping"]["save_path"])):
     f_names = os.listdir(genius_path)
     for art_file in f_names:
         artist_path = genius_path / art_file
-        a = MyArtist(pickle.load(open(artist_path, 'rb')))
-        yield a     
+        a = MyArtist(pickle.load(open(artist_path, "rb")))
+        yield a
 
-def load_jsonl(p:Path):
-    with open(p, 'r') as f:
+
+def load_jsonl(p: Path):
+    with open(p) as f:
         while line := f.readline():
             yield json.loads(line)
 
+
 # for legacy reasons (i am retarded)this is outside this should be in the save_artist_to_pkl
-def sanitize_art_name(name:str)-> str:
-    return  name.replace(' ', '_').replace(".", "_").replace('/', ' ').replace('?', ' ').strip()
+def sanitize_art_name(name: str) -> str:
+    return name.replace(" ", "_").replace(".", "_").replace("/", " ").replace("?", " ").strip()
+
+
 # def to_pkl(
 #         artist: MyArtist,
 #         artist_name: str,
@@ -71,18 +79,19 @@ def sanitize_art_name(name:str)-> str:
 #         with open(out_path / (artist_name + suffix), 'wb') as f:
 #             pickle.dump(artist, f)
 
+
 def get_wsd_data():
-    """for all artists return a tuple (file name, list) 
+    """for all artists return a tuple (file name, list)
     where list has wsd for each song"""
-    path = Path(CONFIG['wsd_outputs'])
+    path = Path(CONFIG["wsd_outputs"])
     wsd_files = [f for f in os.listdir(path) if os.path.isfile(path / f)]
     for wsd_file in wsd_files:
         result = []
         for song in load_jsonl(path / wsd_file):
-            result.append(song['wsd'])
+            result.append(song["wsd"])
         yield (wsd_file, result)
 
+
 def get_stopwords():
-    path = CONFIG['stopwords']
+    path = CONFIG["stopwords"]
     return [line.rstrip() for line in open(path)]
-        
