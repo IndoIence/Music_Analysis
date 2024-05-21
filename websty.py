67f365a13a5de1d8b2f24ee2b7c301a2f1db4fa9
tadple <tadeusz.plesniak@gmail.com>
2024-05-21 14:19:35 +0200

    pre-commit test2

--- websty.py
+++ websty.py
@@ -16,16 +16,16 @@ def get_websty_input(one_artist_file = False, words_limit = 10000):
         f_name = sanitize_art_name(a_name)
         with open(in_dir / (f_name + '.artPkl'), 'rb') as f:
             artist = pickle.load(f)
-        out = ''
         songs = artist.get_limit_songs(limit = words_limit, only_art=True)
         # save to file based on the format
         if one_artist_file:
+            out = ''            
             for i, song in enumerate(songs):
                 out += str(i) + '\n' + song.clean_song_text() + '\n'
             open(out_dir / (f_name + '.txt'), 'w').write(out)
         else:
             for song in songs:
-                open(out_dir / (f_name + '_' + sanitize_art_name(song.title) + '.txt'), 'w').write(out)
+                open(out_dir / (f_name + '_' + sanitize_art_name(song.title) + '.txt'), 'w').write(song.clean_song_text())
 
 # %%
 
@@ -37,5 +37,5 @@ def zip_websty():
         for ff in (f for f in out_dir.iterdir() if f.is_file() and f.suffix == '.txt'):
             zf.write(ff, ff.name)
 
-get_websty_input(True, words_limit=30000)
+get_websty_input(False, words_limit=20000)
 zip_websty()
