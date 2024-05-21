3fe006d382ada5d6028e014665ff79e72e873f1b
tadple <tadeusz.plesniak@gmail.com>
2024-05-21 16:21:03 +0200

    i fixed the install-types in mypy i am losing it oh my god

--- websty.py
+++ websty.py
@@ -5,8 +5,9 @@ import pickle
 import os
 import zipfile
 
+
 # %%
-def get_websty_input(one_artist_file = False, words_limit = 10000):
+def get_websty_input(one_artist_file=False, words_limit=10000):
     in_dir = Path(CONFIG["artists_pl_path"])
     out_dir = Path(CONFIG["websty_path"])
     [os.remove(f) for f in out_dir.iterdir() if f.is_file]
@@ -14,28 +15,33 @@ def get_websty_input(one_artist_file = False, words_limit = 10000):
     artist_names = CONFIG["artist_names"]
     for a_name in artist_names:
         f_name = sanitize_art_name(a_name)
-        with open(in_dir / (f_name + '.artPkl'), 'rb') as f:
+        with open(in_dir / (f_name + ".artPkl"), "rb") as f:
             artist = pickle.load(f)
-        songs = artist.get_limit_songs(limit = words_limit, only_art=True)
+        songs = artist.get_limit_songs(limit=words_limit, only_art=True)
         # save to file based on the format
         if one_artist_file:
-            out = ''            
+            out = ""
             for i, song in enumerate(songs):
-                out += str(i) + '\n' + song.clean_song_text() + '\n'
-            open(out_dir / (f_name + '.txt'), 'w').write(out)
+                out += str(i) + "\n" + song.clean_song_text() + "\n"
+            open(out_dir / (f_name + ".txt"), "w").write(out)
         else:
             for song in songs:
-                open(out_dir / (f_name + '_' + sanitize_art_name(song.title) + '.txt'), 'w').write(song.clean_song_text())
+                open(out_dir / (f_name + "_" + sanitize_art_name(song.title) + ".txt"), "w").write(
+                    song.clean_song_text()
+                )
+
 
 # %%
+
 
 def zip_websty():
     out_dir = Path(CONFIG["websty_path"])
 
-    zip_name = 'websty.zip'
-    with zipfile.ZipFile(Path(CONFIG["websty_path"]) / zip_name, 'w', zipfile.ZIP_DEFLATED) as zf:
-        for ff in (f for f in out_dir.iterdir() if f.is_file() and f.suffix == '.txt'):
+    zip_name = "websty.zip"
+    with zipfile.ZipFile(Path(CONFIG["websty_path"]) / zip_name, "w", zipfile.ZIP_DEFLATED) as zf:
+        for ff in (f for f in out_dir.iterdir() if f.is_file() and f.suffix == ".txt"):
             zf.write(ff, ff.name)
+
 
 get_websty_input(False, words_limit=20000)
 zip_websty()
